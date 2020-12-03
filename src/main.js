8bd967f9df96e3586fffb8b41998cf165888d270
ofutonMizuki <mizuki@ofuton.net>
2020-12-04 02:17:18 +0900

    なんかおかしかった

--- src/main.js
+++ src/main.js
@@ -126,16 +126,12 @@ window.onerror = function (message, file, lineNo, colNo, error) {
         file + ': ' + lineNo + ': ' + colNo + '\n' + (error != null ? error.stack : 'エラーオブジェクトは存在しません'));
 }
 
-<<<<<<< HEAD
 window.onload = function () {
     main();
 }
-=======
+
 //エラーを検知したときにメッセージを吐きます
 window.onerror = function (message, file, lineNo, colNo, error) {
     alert(message + '\n' +
         file + ': ' + lineNo + ': ' + colNo + '\n' + (error != null ? error.stack : 'エラーオブジェクトは存在しません'));
 }
-
-main();
->>>>>>> parent of 7865b0d (Revert "合法手の生成の高速化")
